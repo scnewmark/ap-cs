9820161dbe199134e8d065a4aa60402309d41a43
Samuel Newmark <samuel.newmark10@gmail.com>
2020-09-24 07:16:49 -0500

    added invocations code and tasking code

---
+++ Java/Tasking/Task.java
@@ -0,0 +1,25 @@
+package Java.Tasking;
+
+public class Task {
+
+    final static int DRIPS_PER_GALLON = 15140;
+
+    public static int favorite() {
+        int value = ((((16 * 65) + 12) * 72) / 68);
+        int result = value - 1;
+        return result;
+    }
+
+    public static int wasted(double r, int d) {
+        double drips = r * (1440 * d);
+        int wastedGallons = (int) Math.round(drips / DRIPS_PER_GALLON);
+        return wastedGallons;
+    }
+
+    public static int count(double a, double b, int mL) {
+        double volume = 5 * Math.PI * a * ((b * b) / 24);
+        double containsBeans = 0.698 * mL;
+        double totalBeans = containsBeans / volume;
+        return (int)totalBeans;
+    }
+}
