4ee38f0bc7e651c5953ed4b68274c558194dd720
Sap ★ <samuel.newmark10@gmail.com>
2020-11-02 16:15:35 -0600

    Deal lab completed

---
+++ Java/Deal/Deal.java
@@ -0,0 +1,36 @@
+package Deal;
+
+public class Deal {
+    private int prizeLoc;
+    private int userGuess;
+
+    public Deal() {
+        setPrizeLoc();
+        setUserGuess();
+    }
+
+    public void setUserGuess() {
+        userGuess = randomNum();
+    }
+
+    public void setPrizeLoc() {
+        prizeLoc = randomNum();
+    }
+
+    public int getUserGuess() {
+        return userGuess;
+    }
+
+    public int getPrizeLoc() {
+        return prizeLoc;
+    }
+
+    public int randomNum() {
+        return (int) Math.floor(Math.random() * 3) + 1;
+    }
+
+    @Override 
+    public String toString() {
+        return "user guess: " + userGuess + " prizeLoc: " + prizeLoc;
+    }
+}
