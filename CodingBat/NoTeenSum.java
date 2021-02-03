7f8f13a98ddb80ebc3e7e9705737fd71e9c5b103
Sap ★ <samuel.newmark10@gmail.com>
2021-02-02 19:10:54 -0600

    Upload NoTeenSum and OverTop42 CodingBat problems

---
+++ CodingBat/NoTeenSum.java
@@ -0,0 +1,23 @@
+package CodingBat;
+
+public class NoTeenSum {
+    public static int noTeenSum(int a, int b, int c) {
+        a = fixTeen(a);
+        b = fixTeen(b);
+        c = fixTeen(c);
+        return a + b + c;
+    }
+
+    public static int fixTeen(int n) {
+        if (n >= 13 && n <= 19) {
+          return  n == 15 || n == 16 ? n : 0;
+        }
+        return n;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(noTeenSum(1, 2, 3));
+        System.out.println(noTeenSum(2, 13, 1));
+        System.out.println(noTeenSum(2, 1, 14));
+    }
+}
---
+++ CodingBat/OverTop42.java
@@ -0,0 +1,24 @@
+package CodingBat;
+
+public class OverTop42 {
+    public static int overTop42(int a, int b) {
+        if (a > 42 && b > 42) {
+            return 0;
+        } else if (a > 42) {
+            return b;
+        } else if (b > 42) {
+            return a;
+        }
+
+        final int distanceA = 42 - a;
+        final int distanceB = 42 - b;
+        return distanceB > distanceA ? a : b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(overTop42(29, 21));
+        System.out.println(overTop42(47, 62));
+        System.out.println(overTop42(55, 46));
+    }
+
+}
