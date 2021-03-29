0ad70955fee818ea6a16f95d62a0874d14a93500
Sap ★ <samuel.newmark10@gmail.com>
2021-03-29 11:17:34 -0500

    Add Localization lab

---
+++ Labs/Localization/Canada.java
@@ -0,0 +1,30 @@
+package Labs.Localization;
+
+public class Canada extends AbstractLocale {
+    private final static double EXCHANGE_RATE = 1.26;
+    private final static int CST_OFFSET = +1;
+
+    public Canada() {
+        super("Canada", "French");
+    }
+
+    @Override
+    public String getGreeting() {
+        return "Bonjour";
+    }
+
+    @Override
+    public String getLocalTime(int hour, int min) {
+        return String.format(String.format("%d:%02d", hour + CST_OFFSET, min));
+    }
+
+    @Override
+    public String getCurrencySymbol() {
+        return "C$";
+    }
+
+    @Override
+    public double getCurrencyValue(final double dollarAmount) {
+        return (double) dollarAmount * EXCHANGE_RATE;
+    }
+}
