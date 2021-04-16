bb1de9cd679b7b298e3889644996af583ccd02a6
Sap ★ <samuel.newmark10@gmail.com>
2021-04-16 13:07:29 -0500

    Add CountTriple CodingBat problem

---
+++ CodingBat/CountTriple.java
@@ -0,0 +1,36 @@
+package CodingBat;
+
+public class CountTriple {
+    public static void main(String[] args) {
+        System.out.println(countTriple("xxxabyyyycd")); // 3
+    }
+
+    public static int countTriple(final String str) {
+        final char[] ch = str.toCharArray();
+        if (ch.length < 3) return 0;
+        final int count = count(ch, 0, 3, 0);
+        return count;
+    }
+
+    public static int count(final char[] arr, int start, int end, int count) {
+        final int threshold = arr.length + 1;
+        if (end != threshold) {
+            final char[] ch = slice(arr, start, end);
+            if (ch[0] == ch[1] && ch[1] == ch[2]) {
+                count++;
+            }
+            return count(arr, start+1, end+1, count);
+        }
+        return count;
+    }
+
+    public static char[] slice(final char[] arr, final int start, final int end) {
+        final char[] c = new char[end-start];
+        int index = 0;
+        for (int i = start; i < end; i++) {
+            c[index] = arr[i];
+            index++;
+        }
+        return c;
+    }
+}
