4e2b2df10f98b2fd2b49dff7d9eb1769d424ecff
Sap ★ <samuel.newmark10@gmail.com>
2021-04-30 12:47:20 -0500

    Restore original HelloWorld.java

--- src/main/java/com/labs/HelloWorld.java
+++ src/main/java/com/labs/HelloWorld.java
@@ -2,12 +2,6 @@ package com.labs;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(fun(7));
-    }
-
-    public static int fun(int x){
-        if(x < 4)
-            return 5;
-        return x+fun(x-1)+fun(x-3);
+        System.out.println("Hello world!");
     }
 }
